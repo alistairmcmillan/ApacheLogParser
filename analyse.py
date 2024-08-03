8604630a17c278e57c12e4a1fe212a632a423450
Alistair McMillan <alistairmcmillan75@gmail.com>
2024-08-04 00:10:41 +0100

    Start HTML output

--- analyse.py
+++ analyse.py
@@ -1,3 +1,4 @@
+import datetime
 import operator
 
 address_dict = {}
@@ -54,10 +55,23 @@ with open(filename, 'r', encoding='UTF-8') as file:
             current_count = resource_dict[line_new[3]]
             resource_dict[line_new[3]] = current_count+1
 
-    print('\nVISITED RESOURCES')
-    for resource in reversed(sorted(resource_dict.items(), key=operator.itemgetter(1))):
-        print(resource)
+    d = datetime.datetime.now()
+    filename_output = (f'report {d.year}{d.month:02d}{d.day:02d}'
+                       f'{d.hour:02d}{d.minute:02d}{d.second:02d}.html')
+    with open(filename_output, 'w') as f:
+        f.write('<html><head>')
+        f.write('<style>table { font-size: 10px !important; }</style>')
+        f.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">')
+        f.write('</head><body>\n')
+        f.write('<table class="table" style="margin-left: auto; margin-right: auto; width: 80%">\n')
+        f.write('<thead><tr><th>Resource</th><th>Visitor Count</th></tr></thead>\n')
+        f.write('<tbody>\n')
+        for resource in reversed(sorted(resource_dict.items(), key=operator.itemgetter(1))):
+            f.write(f'<td>https://escwiki{resource[0]}</td><td>{resource[1]}</td></tr>\n')
+        f.write('</tbody>\n')
+        f.write('</table>\n')
+        f.write('</body></html>\n')
 
-    print('\nVISITOR ADDRESSES')
-    for address in reversed(sorted(address_dict.items(), key=operator.itemgetter(1))):
-        print(address)
+#    print('\nVISITOR ADDRESSES')
+#    for address in reversed(sorted(address_dict.items(), key=operator.itemgetter(1))):
+#        print(address)
